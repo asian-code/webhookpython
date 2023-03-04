52ffa9da33b67eca50ba2cc0fe96c8dabda95e44
asian-code <123rex100@gmail.com>
2023-03-04 17:50:34 -0500

    update

--- listener.py
+++ listener.py
@@ -1,10 +1,28 @@
 from flask import Flask, request, abort
+import subprocess
+
+# import json
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
+@app.route('/home', methods=['POST'])
+def home():
     if request.method == 'POST':
+        data=request.json
+        print(data["name"])
+        return 'success', 200
+    else:
+        abort(400)
+
+@app.route('/work', methods=['POST'])
+def work():
+    if request.method == 'POST':
+        data=request.json
+        #check for what program to run
+        if (data["name"]=="freshdesk-auth-tool"):
+            print("testing")
+    elif (data["name"]=="create VM"):
+    
         print(request.json)
         return 'success', 200
     else:
@@ -12,3 +30,11 @@ def webhook():
 
 if __name__ == '__main__':
     app.run()
+
+
+
+# Run another Python script and capture the output
+result = subprocess.run(['python', 'path/to/other_script.py'], capture_output=True)
+
+# Print the output
+print(result.stdout)
--- sender.py
+++ sender.py
@@ -1,8 +1,8 @@
 import requests
 import json
-url = 'http://127.0.0.1:5000/webhook'
-
-data = { 'name': 'Testing', 
+url = 'http://127.0.0.1:5000/home'
+url2="http://127.0.0.1:5000/freshdesk"
+data = { 'name': 'Testing name data', 
          'message': 'message' }
 
 r = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
