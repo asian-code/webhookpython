90b2f269d312e91a414135185079ca3d83cb82b7
asian-code <52612730+asian-code@users.noreply.github.com>
2023-03-08 13:29:46 -0500

    Update sender.py
    testing url links

--- sender.py
+++ sender.py
@@ -1,11 +1,21 @@
 import requests
 import json
-url = 'http://127.0.0.1:5000/home'
-url2="http://127.0.0.1:5000/freshdesk"
-data = { 'name': 'Testing name data', 
-         'message': 'message' }
-
-r = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
-
+url = 'http://127.0.0.1:5000/work'
+dns='http://test.eric-n.com/work'
+ip='http://10.10.100.198:5000/work'
+data = { 'name': 'freshdesk-auth-tool', 
+         'message': 'test message' }
 
 
+data2 = { 'name': 'create VM', 
+         'message': 'test message' }
+
+
+def sendPOST(url,message):
+    print("Sending to "+url,end=" - ")
+    r = requests.post(url, data=json.dumps(message), headers={'Content-Type': 'application/json'})
+    print(r.status_code)
+
+sendPOST(url,data)
+sendPOST(dns,data)
+sendPOST(ip,data)
