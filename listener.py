7be03207fbfb9f0d20d77f84dfa7d0ba43e8468f
asian-code <52612730+asian-code@users.noreply.github.com>
2023-03-08 13:30:33 -0500

    Update listener.py
    testing why request are only GET instead of POST

--- listener.py
+++ listener.py
@@ -5,36 +5,32 @@ import subprocess
 
 app = Flask(__name__)
 
-@app.route('/home', methods=['POST'])
-def home():
-    if request.method == 'POST':
-        data=request.json
-        print(data["name"])
-        return 'success', 200
-    else:
-        abort(400)
-
 @app.route('/work', methods=['POST'])
 def work():
-    if request.method == 'POST':
-        data=request.json
+    print('WOOOHOOOOO')
+    data=request.json
+    print(data)
+    if request.method == 'POST':#'POST':
+        
         #check for what program to run
         if (data["name"]=="freshdesk-auth-tool"):
-            print("testing")
-    elif (data["name"]=="create VM"):
-    
-        print(request.json)
+            print("Running freshdesk-auth-tool ")
+            # Run another Python script and capture the output
+            # result = subprocess.run(['python3', '/home/eric/Downloads/FreshdeskReAuthChecker.py'], capture_output=True)
+            # print(result.stdout)
+
+        elif (data["name"]=="create VM"):
+            print("VM WOOOHOOO")
+            print("data: "+request.json)
         return 'success', 200
     else:
+        print(request.json)
+        print('HEHEHEHEHEHEHEHEHHEEH')
         abort(400)
-
+    print('END of function')
 if __name__ == '__main__':
     app.run()
 
 
 
-# Run another Python script and capture the output
-result = subprocess.run(['python', 'path/to/other_script.py'], capture_output=True)
 
-# Print the output
-print(result.stdout)
