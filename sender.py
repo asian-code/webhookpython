ee11f3934d6bc1129a1c800b1f91b77e08369321
asian-code <52612730+asian-code@users.noreply.github.com>
2023-03-14 13:42:18 -0400

    Update sender.py
    have both GET and POST functions for testing

--- sender.py
+++ sender.py
@@ -1,7 +1,8 @@
 import requests
 import json
 url = 'http://127.0.0.1:5000/work'
-dns='http://test.eric-n.com/work'
+dns='https://test.eric-n.com/work/freshdesk-auth-tool'
+dns2='https://test.eric-n.com/work'
 ip='http://10.10.100.198:5000/work'
 data = { 'name': 'freshdesk-auth-tool', 
          'message': 'test message' }
@@ -16,6 +17,12 @@ def sendPOST(url,message):
     r = requests.post(url, data=json.dumps(message), headers={'Content-Type': 'application/json'})
     print(r.status_code)
 
-sendPOST(url,data)
-sendPOST(dns,data)
-sendPOST(ip,data)
+def sendGET(url,message):
+    print("Sending to "+url,end=" - ")
+    r = requests.get(url, data=json.dumps(message), headers={'Content-Type': 'application/json'})
+    print(r.status_code)
+
+
+# sendPOST(url,data)
+sendPOST(dns2,data)
+# sendGET(dns2,data)
